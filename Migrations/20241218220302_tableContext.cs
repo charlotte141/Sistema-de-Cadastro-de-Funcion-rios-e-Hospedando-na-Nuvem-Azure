878c35287e5e484d712003355cb7a06b2b64f332
Delcio_Santos <87926383+charlotte141@users.noreply.github.com>
2024-12-18 23:20:39 +0100

    projecto concluido

---
+++ Migrations/20241218220302_tableContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sistema_de_Cadastro_de_Funcion_rios_e_Hospedando_na_Nuvem_Azure.Migrations
+{
+    /// <inheritdoc />
+    public partial class tableContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
